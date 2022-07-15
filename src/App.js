b0e059c7eaf5ce48c9da81a4f0794e1f6d83c7b5
Muhammad Umair <181370124@gift.edu.pk>
2022-07-15 12:30:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import React, { useState } from 'react';
 
 const App = () => {
+
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setTime] = useState(time);
+  
+  const updateTime = () => {
+    let ctime = new Date().toLocaleTimeString();
+    setTime(ctime);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Time</h1>
-        <button>Get Time</button>
+        <h1>{ctime}</h1>
+        <button onClick={updateTime}>Get Time</button>
       </header>
     </div>
   );
